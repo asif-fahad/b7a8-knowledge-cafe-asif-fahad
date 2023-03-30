68d9047014aea7e3baaa58bd37c7c8f5089dba4b
asif-fahad <asif123fahad@gmail.comm>
2023-03-30 23:50:43 +0600

    header added

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
 import React from 'react';
+import Header from './components/Header/Header';
+
 
 const App = () => {
   return (
     <div>
-      <button className="btn">Button</button>
-      <button className="btn btn-primary">Button</button>
-      <button className="btn btn-secondary">Button</button>
-      <button className="btn btn-accent">Button</button>
-      <button className="btn btn-ghost">Button</button>
-      <button className="btn btn-link">Button</button>
+      <Header></Header>
     </div>
   );
 };
